b71ee69e64040cce40e4f27d8149836a365c401b
yuu2510jp <yuu2510jp@gmail.com>
2018-07-22 17:37:02 +0900

    first commit

---
+++ src/main/java/com/Ideng/SpringBootDomoApplication.java
@@ -0,0 +1,12 @@
+package com.Ideng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDomoApplication.class, args);
+	}
+}
