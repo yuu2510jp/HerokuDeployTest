ed07d5229a53114c859ecf3a261a826b0e4a8dc1
yuu2510jp <yuu2510jp@gmail.com>
2018-08-06 23:46:32 +0900

    変更点

--- src/main/java/com/Ideng/controller/HomeController.java
+++ src/main/java/com/Ideng/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.Ideng.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@RequestMapping("/")
 	public String home() {
-		return "hello world!!!!";
+		return "TP_Summary";
 	}
 }
